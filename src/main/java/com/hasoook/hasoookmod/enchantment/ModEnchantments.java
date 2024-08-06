b2a0346ab05f1764fa1e69424b185d809bf67846
Hasok <2656053560@qq.com>
2024-08-06 14:56:52 +0800

    分离爆炸

--- src/main/java/com/hasoook/hasoookmod/enchantment/ModEnchantments.java
+++ src/main/java/com/hasoook/hasoookmod/enchantment/ModEnchantments.java
@@ -16,6 +16,7 @@ import net.minecraft.world.level.block.Block;
 public class ModEnchantments {
     // 自定义附魔资源键
     public static final ResourceKey<Enchantment> RANDOM_BULLETS = key("random_bullets");
+    public static final ResourceKey<Enchantment> SEPARATION_EXPLOSION = key("separation_explosion");
 
     // 引导方法，用于初始化附魔注册
     public static <DamageType> void bootstrap(BootstrapContext<Enchantment> context)
@@ -27,12 +28,27 @@ public class ModEnchantments {
         HolderGetter<Block> holdergetter3 = context.lookup(Registries.BLOCK);
 
         // 注册自定义附魔
-        register(//亚当
+        register( // 随机子弹
                 context,
                 RANDOM_BULLETS,
                 Enchantment.enchantment(
                         Enchantment.definition(
-                                holdergetter2.getOrThrow(ItemTags.MACE_ENCHANTABLE),
+                                holdergetter2.getOrThrow(ItemTags.BOW_ENCHANTABLE),
+                                2,
+                                1,
+                                Enchantment.constantCost(25),
+                                Enchantment.constantCost(50),
+                                8,
+                                EquipmentSlotGroup.MAINHAND
+                        )
+                )
+        );
+        register( // 分离爆炸
+                context,
+                SEPARATION_EXPLOSION,
+                Enchantment.enchantment(
+                        Enchantment.definition(
+                                holdergetter2.getOrThrow(ItemTags.SWORD_ENCHANTABLE),
                                 2,
                                 1,
                                 Enchantment.constantCost(25),
