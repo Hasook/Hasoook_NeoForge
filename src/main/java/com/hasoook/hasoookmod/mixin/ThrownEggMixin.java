24706779d155d0969a0137d79c9ec1cd6da53ae9
Hasok <2656053560@qq.com>
2024-08-12 00:43:20 +0800

    时运鸡蛋

---
+++ src/main/java/com/hasoook/hasoookmod/mixin/ThrownEggMixin.java
@@ -0,0 +1,58 @@
+package com.hasoook.hasoookmod.mixin;
+
+import com.hasoook.hasoookmod.enchantment.ModEnchantmentHelper;
+import com.hasoook.hasoookmod.enchantment.ModEnchantments;
+import net.minecraft.world.entity.EntityDimensions;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.animal.Chicken;
+import net.minecraft.world.entity.projectile.ThrowableItemProjectile;
+import net.minecraft.world.entity.projectile.ThrownEgg;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.enchantment.Enchantments;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.HitResult;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+
+@Mixin(ThrownEgg.class)
+public abstract class ThrownEggMixin extends ThrowableItemProjectile {
+    public ThrownEggMixin(EntityType<? extends ThrowableItemProjectile> pEntityType, Level pLevel) {
+        super(pEntityType, pLevel);
+    }
+
+    @Unique
+    private static final EntityDimensions ZERO_SIZED_DIMENSIONS = EntityDimensions.fixed(0.0F, 0.0F);
+
+    @Override
+    protected void onHit(HitResult pResult) {
+        super.onHit(pResult);
+        if (!this.level().isClientSide) {
+            ItemStack itemStack = this.getItem(); // 获取鸡蛋的 ItemStack
+            int fortuneLevel = ModEnchantmentHelper.getEnchantmentLevel(Enchantments.FORTUNE, itemStack);
+            // 获取物品的“时运”等级
+            if (this.random.nextInt(8) == 0) {
+                int i = 1;
+                if (this.random.nextInt(32) == 0) {
+                    i = 4;
+                }
+
+                for (int j = 0; j < i + fortuneLevel * 100; j++) {
+                    Chicken chicken = EntityType.CHICKEN.create(this.level());
+                    if (chicken != null) {
+                        chicken.setAge(-24000);
+                        chicken.moveTo(this.getX(), this.getY(), this.getZ(), this.getYRot(), 0.0F);
+                        if (!chicken.fudgePositionAfterSizeChange(ZERO_SIZED_DIMENSIONS)) {
+                            break;
+                        }
+
+                        this.level().addFreshEntity(chicken);
+                    }
+                }
+            }
+
+            this.level().broadcastEntityEvent(this, (byte)3);
+            this.discard();
+        }
+    }
+
+}
