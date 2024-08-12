26286ad547146bbb16214e3c035a4f4118ef0c05
Hasok <2656053560@qq.com>
2024-08-12 15:18:39 +0800

    连锁打怪

--- src/main/java/com/hasoook/hasoookmod/event/enchantment/chain_damage.java
+++ src/main/java/com/hasoook/hasoookmod/event/enchantment/chain_damage.java
@@ -15,30 +15,41 @@ import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.entity.living.LivingIncomingDamageEvent;
 
 import java.util.List;
+import java.util.Objects;
 
 @EventBusSubscriber(modid = HasoookMod.MODID)
 public class chain_damage {
+    private static final String CHAIN_DAMAGE_TAG = "hasoookmod:chain_damage";
+
     @SubscribeEvent
     public static void onEntityAttack(LivingIncomingDamageEvent event) {
-        LivingEntity target = event.getEntity(); // 获取实体
-        Entity sourceEntity = event.getSource().getEntity(); // 获取攻击者
+        LivingEntity target = event.getEntity();
+        Entity sourceEntity = event.getSource().getEntity();
 
         if (sourceEntity instanceof LivingEntity attacker) {
-            ItemStack attackerMainHandItem = attacker.getMainHandItem(); // 获取攻击者的主手物品
+            ItemStack attackerMainHandItem = attacker.getMainHandItem();
             int swapLevel = ModEnchantmentHelper.getEnchantmentLevel(ModEnchantments.CHAIN_DAMAGE, attackerMainHandItem);
 
             if (swapLevel > 0) {
                 float damage = event.getAmount();
-                Level world = target.level(); // 获取当前世界对象
-                double radius = 5.0; // 设置半径值
+                Level world = target.level();
+                double radius = 5.0;
 
                 // 获取周围与目标实体相同类型的实体
                 AABB searchBox = target.getBoundingBox().inflate(radius);
                 List<LivingEntity> nearbyEntities = world.getEntitiesOfClass(LivingEntity.class, searchBox, e -> e.getClass().equals(target.getClass()));
 
                 for (LivingEntity entity : nearbyEntities) {
-                    if (entity != target) { // 不对目标自身造成伤害
-                        entity.setHealth(0);
+                    if (entity != target && entity != sourceEntity) {
+                        boolean hasTag = entity.getPersistentData().getBoolean(CHAIN_DAMAGE_TAG);
+                        if (!hasTag) {
+                            entity.getPersistentData().putBoolean(CHAIN_DAMAGE_TAG, true);
+                            // 给予标记
+                            entity.hurt(sourceEntity.damageSources().thrown(sourceEntity, sourceEntity), damage);
+                            // 造成伤害
+                            Objects.requireNonNull(world.getServer()).execute(() -> entity.getPersistentData().remove(CHAIN_DAMAGE_TAG));
+                            // 清除标记
+                        }
                     }
                 }
             }
