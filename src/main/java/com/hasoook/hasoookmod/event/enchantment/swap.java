0ef5e11b80686da135af845a06839f82a3fa127d
Hasok <2656053560@qq.com>
2024-08-13 18:15:07 +0800

    给予

--- src/main/java/com/hasoook/hasoookmod/event/enchantment/swap.java
+++ src/main/java/com/hasoook/hasoookmod/event/enchantment/swap.java
@@ -3,10 +3,16 @@ package com.hasoook.hasoookmod.event.enchantment;
 import com.hasoook.hasoookmod.HasoookMod;
 import com.hasoook.hasoookmod.enchantment.ModEnchantmentHelper;
 import com.hasoook.hasoookmod.enchantment.ModEnchantments;
+import net.minecraft.core.Holder;
+import net.minecraft.tags.EnchantmentTags;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
+import net.minecraft.world.item.enchantment.Enchantments;
+import net.minecraft.world.item.enchantment.ItemEnchantments;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.entity.living.LivingIncomingDamageEvent;
@@ -25,6 +31,13 @@ public class swap {
             int giveLevel = ModEnchantmentHelper.getEnchantmentLevel(ModEnchantments.GIVE, attackerMainHandItem);
 
             if (giveLevel > 0 && targetMainHandItem.isEmpty()) {
+                if (giveLevel == 1) {
+                    EnchantmentHelper.updateEnchantments(
+                            attackerMainHandItem, p_330066_ -> p_330066_.removeIf(p_344368_ -> p_344368_.is(ModEnchantments.GIVE))
+                    );
+                    // 移除附魔
+                }
+
                 target.setItemInHand(InteractionHand.MAIN_HAND, attackerMainHandItem);
                 attacker.setItemInHand(InteractionHand.MAIN_HAND, ItemStack.EMPTY);
             }
