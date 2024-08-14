0b3c45da97d7df88fb195b1fe82d21710204ff16
Hasok <2656053560@qq.com>
2024-08-14 17:10:11 +0800

    连锁打怪

--- src/main/java/com/hasoook/hasoookmod/event/enchantment/chain_damage.java
+++ src/main/java/com/hasoook/hasoookmod/event/enchantment/chain_damage.java
@@ -10,7 +10,6 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.level.Level;
 import net.neoforged.bus.api.SubscribeEvent;
