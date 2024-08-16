ffedba104b49972caf0ab6e917f74528d43f84d9
Hasok <2656053560@qq.com>
2024-08-17 01:04:38 +0800

    时运鸡蛋

--- src/main/java/com/hasoook/hasoookmod/mixin/ThrownEggMixin.java
+++ src/main/java/com/hasoook/hasoookmod/mixin/ThrownEggMixin.java
@@ -4,6 +4,8 @@ import com.hasoook.hasoookmod.enchantment.ModEnchantmentHelper;
 import com.hasoook.hasoookmod.enchantment.ModEnchantments;
 import net.minecraft.world.entity.EntityDimensions;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.animal.Chicken;
 import net.minecraft.world.entity.projectile.ThrowableItemProjectile;
@@ -11,9 +13,12 @@ import net.minecraft.world.entity.projectile.ThrownEgg;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.storage.loot.functions.SetAttributesFunction;
 import net.minecraft.world.phys.HitResult;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
+
+import java.util.Objects;
 
 @Mixin(ThrownEgg.class)
 public abstract class ThrownEggMixin extends ThrowableItemProjectile {
@@ -41,6 +46,7 @@ public abstract class ThrownEggMixin extends ThrowableItemProjectile {
                     Chicken chicken = EntityType.CHICKEN.create(this.level());
                     if (chicken != null) {
                         chicken.setAge(-24000);
+                        Objects.requireNonNull(chicken.getAttribute(Attributes.SCALE)).setBaseValue(this.random.nextInt(2) + 1);
                         chicken.moveTo(this.getX(), this.getY(), this.getZ(), this.getYRot(), 0.0F);
                         if (!chicken.fudgePositionAfterSizeChange(ZERO_SIZED_DIMENSIONS)) {
                             break;
