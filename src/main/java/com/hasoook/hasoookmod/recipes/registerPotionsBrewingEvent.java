a0c0fb222f7652fd6b9c553ff18a67fb92e85efb
Hasok <2656053560@qq.com>
2024-08-18 22:06:49 +0800

    混乱

---
+++ src/main/java/com/hasoook/hasoookmod/recipes/registerPotionsBrewingEvent.java
@@ -0,0 +1,22 @@
+package com.hasoook.hasoookmod.recipes;
+
+import com.hasoook.hasoookmod.HasoookMod;
+import com.hasoook.hasoookmod.block.ModBlock;
+import com.hasoook.hasoookmod.effect.ModPotions;
+import net.minecraft.world.item.alchemy.PotionBrewing;
+import net.minecraft.world.item.alchemy.Potions;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.brewing.RegisterBrewingRecipesEvent;// 使用某种方法监听事件
+
+@EventBusSubscriber(modid = HasoookMod.MODID)
+public class registerPotionsBrewingEvent {
+    @SubscribeEvent
+    public static void onRegisterPotionsBrewing(RegisterBrewingRecipesEvent event) {
+        PotionBrewing.Builder builder = event.getBuilder();
+
+        builder.addMix(Potions.AWKWARD, ModBlock.CONFUSION_FLOWER.asItem(), ModPotions.CONFUSION);
+        // 混乱药水
+    }
+
+}
