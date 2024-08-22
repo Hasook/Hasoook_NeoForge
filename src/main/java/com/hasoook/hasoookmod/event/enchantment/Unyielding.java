0d68ba68903f6404168e4656877dd6c730239b3b
Hasok <2656053560@qq.com>
2024-08-23 01:31:17 +0800

    不屈

--- src/main/java/com/hasoook/hasoookmod/event/enchantment/Unyielding.java
+++ src/main/java/com/hasoook/hasoookmod/event/enchantment/Unyielding.java
@@ -4,6 +4,7 @@ import com.hasoook.hasoookmod.HasoookMod;
 import com.hasoook.hasoookmod.effect.ModEffects;
 import com.hasoook.hasoookmod.enchantment.ModEnchantmentHelper;
 import com.hasoook.hasoookmod.enchantment.ModEnchantments;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.damagesource.DamageType;
 import net.minecraft.world.effect.MobEffectInstance;
@@ -24,25 +25,30 @@ public class Unyielding {
         boolean mainHand = entity.getMainHandItem().getItem() == Items.TOTEM_OF_UNDYING; // 检查主手物品
         boolean offHand  = entity.getOffhandItem().getItem() == Items.TOTEM_OF_UNDYING; // 检查副手物品
         int unyieldingLevel = ModEnchantmentHelper.getEnchantmentLevel(ModEnchantments.UNYIELDING, chestplate);
-        double amount = event.getAmount();
-        double health = entity.getHealth() + entity.getAbsorptionAmount();
-        String source = event.getSource().getMsgId();
-        MobEffectInstance strengthEffect = entity.getEffect(ModEffects.UNYIELDING);
+        double amount = event.getAmount(); // 获取伤害值
+        double health = entity.getHealth() + entity.getAbsorptionAmount(); // 获取生命值+伤害吸收值
+        String source = event.getSource().getMsgId(); // 获取伤害类型
+        MobEffectInstance strengthEffect = entity.getEffect(ModEffects.UNYIELDING); // 获取药水效果
         int strengthLevel = (strengthEffect != null) ? strengthEffect.getAmplifier() + 1 : 0;
 
         if (unyieldingLevel > 0 && amount > health && strengthEffect == null && !source.equals("genericKill") && !mainHand && !offHand) {
             entity.setHealth(0.1F); // 将生命设置为0.1
             entity.setAbsorptionAmount(0); // 将生命吸收血量设置为0
             event.setAmount(0); // 将伤害设置为0
-
             MobEffectInstance effect = new MobEffectInstance(ModEffects.UNYIELDING, 100, 0);
             entity.addEffect(effect); // 给予不屈效果
         }
 
         if (strengthLevel > 0 && !source.equals("genericKill")) {
-            entity.setHealth(0.1F); // 将生命设置为0.1
-            entity.setAbsorptionAmount(0); // 将生命吸收血量设置为0
-            event.setAmount(0); // 将伤害设置为0
+            /* CompoundTag nbt = entity.getPersistentData();
+            double totalDamage = nbt.getDouble("LastDamageValue"); // 获取LastDamageValue的值
+            totalDamage += amount; // 加上受到的伤害值
+            nbt.putDouble("LastDamageValue", totalDamage); // 设置nbt值
+            System.out.println(nbt.getDouble("LastDamageValue")); */
+
+            entity.setHealth(0.1F);
+            entity.setAbsorptionAmount(0);
+            event.setAmount(0);
         }
     }
 
