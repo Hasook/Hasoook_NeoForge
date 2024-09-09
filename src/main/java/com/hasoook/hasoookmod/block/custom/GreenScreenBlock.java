e0b4815fd84d498054a3f7e9ca6def5035d2f75c
Hasok <2656053560@qq.com>
2024-09-09 18:02:55 +0800

    绿幕方块

--- src/main/java/com/hasoook/hasoookmod/block/custom/GreenScreenBlock.java
+++ src/main/java/com/hasoook/hasoookmod/block/custom/GreenScreenBlock.java
@@ -4,6 +4,7 @@ import net.minecraft.world.level.block.Block;
 
 public class GreenScreenBlock extends Block {
     public GreenScreenBlock(Properties p_49795_) {
-        super(p_49795_);
+        super(p_49795_.lightLevel((state) -> 15).noOcclusion());
     }
+
 }
