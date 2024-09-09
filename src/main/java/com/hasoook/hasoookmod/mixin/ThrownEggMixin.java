5bbe144fc4491e046a555375a09db50d66cfa12f
Hasok <2656053560@qq.com>
2024-09-09 18:03:09 +0800

    时运鸡蛋

--- src/main/java/com/hasoook/hasoookmod/mixin/ThrownEggMixin.java
+++ src/main/java/com/hasoook/hasoookmod/mixin/ThrownEggMixin.java
@@ -3,7 +3,6 @@ package com.hasoook.hasoookmod.mixin;
 import com.hasoook.hasoookmod.enchantment.ModEnchantmentHelper;
 import net.minecraft.world.entity.EntityDimensions;
 import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.animal.Chicken;
 import net.minecraft.world.entity.projectile.ThrowableItemProjectile;
 import net.minecraft.world.entity.projectile.ThrownEgg;
@@ -11,14 +10,11 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.HitResult;
-import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import java.util.Objects;
 
 @Mixin(ThrownEgg.class)
 public abstract class ThrownEggMixin extends ThrowableItemProjectile {
@@ -29,8 +25,8 @@ public abstract class ThrownEggMixin extends ThrowableItemProjectile {
     @Unique
     private static final EntityDimensions ZERO_SIZED_DIMENSIONS = EntityDimensions.fixed(0.0F, 0.0F);
 
-    @Inject(method = "onHit", at = @At("HEAD"))
-    protected void onHit(HitResult pResult, CallbackInfo ci) {
+    @Override
+    protected void onHit(HitResult pResult) {
         super.onHit(pResult);
         ItemStack itemStack = this.getItem(); // 获取鸡蛋的 ItemStack
         int fortuneLevel = ModEnchantmentHelper.getEnchantmentLevel(Enchantments.FORTUNE, itemStack);
@@ -50,20 +46,20 @@ public abstract class ThrownEggMixin extends ThrowableItemProjectile {
                         chicken.setAge(-24000);
                         chicken.moveTo(this.getX(), this.getY(), this.getZ(), this.getYRot(), 0.0F);
 
-                        /*if (i > 1) { // 如果鸡的数量大于1
+                        if (i > 1) { // 如果鸡的数量大于1
                             double randomX = (this.random.nextDouble() - 0.5) * 0.2;
                             double randomY = (this.random.nextDouble() - 0.5) * 0.2;
                             double randomZ = (this.random.nextDouble() - 0.5) * 0.2;
                             chicken.setDeltaMovement(randomX, randomY, randomZ);
                             // 设置随机方向的运动向量 范围 -0.1 ~ 0.1
-                            double randomJ = (double) j / 6;
+                            double randomJ = (double) j / 6; // 位置偏移
                             chicken.moveTo(this.getX() + randomX * randomJ, this.getY(), this.getZ() + randomZ * randomJ, this.getYRot(), 0.0F);
-                        }*/
+                        }
 
                         if (!chicken.fudgePositionAfterSizeChange(ZERO_SIZED_DIMENSIONS)) {
                             break;
                         }
-                        this.level().addFreshEntity(chicken); // 把鸡添加到世界
+                        this.level().addFreshEntity(chicken);
                     }
                 }
             }
