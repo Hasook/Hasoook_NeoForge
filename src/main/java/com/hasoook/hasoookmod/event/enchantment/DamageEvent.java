8f29f390ba8c0230f1b762ad194a9ae6d61db877
Hasok <2656053560@qq.com>
2024-10-27 22:42:43 +0800

    飞雷神

--- src/main/java/com/hasoook/hasoookmod/event/enchantment/DamageEvent.java
+++ src/main/java/com/hasoook/hasoookmod/event/enchantment/DamageEvent.java
@@ -3,15 +3,11 @@ package com.hasoook.hasoookmod.event.enchantment;
 import com.hasoook.hasoookmod.HasoookMod;
 import com.hasoook.hasoookmod.enchantment.ModEnchantmentHelper;
 import com.hasoook.hasoookmod.enchantment.ModEnchantments;
-import net.minecraft.network.chat.ChatType;
 import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.OutgoingChatMessage;
-import net.minecraft.network.chat.PlayerChatMessage;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.item.enchantment.Enchantments;
@@ -19,7 +15,6 @@ import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.entity.living.LivingIncomingDamageEvent;
 
-import java.awt.*;
 import java.util.Random;
 
 @EventBusSubscriber(modid = HasoookMod.MODID)
