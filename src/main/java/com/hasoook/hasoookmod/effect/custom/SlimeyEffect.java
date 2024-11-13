19e4d176d4a727794a94be3ce36f3d44f2d07d9c
Hasok <2656053560@qq.com>
2024-11-13 18:14:38 +0800

    黏糊糊

--- src/main/java/com/hasoook/hasoookmod/effect/custom/SlimeyEffect.java
+++ src/main/java/com/hasoook/hasoookmod/effect/custom/SlimeyEffect.java
@@ -14,18 +14,23 @@ public class SlimeyEffect extends MobEffect {
 
     @Override
     public boolean applyEffectTick(LivingEntity livingEntity, int amplifier) {
+        // 如果实体与某个表面发生水平方向的碰撞
         if(livingEntity.horizontalCollision) {
+            // 获取实体当前的速度向量
             Vec3 initialVec = livingEntity.getDeltaMovement();
+            // 创建一个新的速度向量，保留水平方向的分量，设置竖直方向的速度为0.2D（使实体能够向上攀爬）
             Vec3 climbVec = new Vec3(initialVec.x, 0.2D, initialVec.z);
+            // 设置实体的速度，`scale(0.96D)` 用于稍微降低实体的速度（模拟黏滑效果）
             livingEntity.setDeltaMovement(climbVec.scale(0.96D));
             return true;
         }
 
+        // 如果没有发生水平方向的碰撞，则调用父类的 applyEffectTick 方法，应用默认的效果逻辑
         return super.applyEffectTick(livingEntity, amplifier);
     }
 
     @Override
     public boolean shouldApplyEffectTickThisTick(int duration, int amplifier) {
-        return true;
+        return true; // 总是应用效果
     }
 }
