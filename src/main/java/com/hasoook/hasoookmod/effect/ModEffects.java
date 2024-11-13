bfb3fd0126ac949c5923297228a2d3b3e0142223
Hasok <2656053560@qq.com>
2024-11-13 17:24:31 +0800

    画

--- src/main/java/com/hasoook/hasoookmod/effect/ModEffects.java
+++ src/main/java/com/hasoook/hasoookmod/effect/ModEffects.java
@@ -12,24 +12,24 @@ import net.minecraft.world.effect.MobEffectCategory;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.neoforged.bus.api.IEventBus;
-import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
-
-import java.util.function.Supplier;
 
 public class ModEffects {
     public static final DeferredRegister<MobEffect> MOB_EFFECTS =
             DeferredRegister.create(BuiltInRegistries.MOB_EFFECT, HasoookMod.MOD_ID);
 
+    // 黏糊糊
     public static final Holder<MobEffect> SLIMEY_EFFECT = MOB_EFFECTS.register("slimey",
             () -> new SlimeyEffect(MobEffectCategory.NEUTRAL, 0x36ebab)
                     .addAttributeModifier(Attributes.MOVEMENT_SPEED,
                             ResourceLocation.fromNamespaceAndPath(HasoookMod.MOD_ID, "slimey"), -0.25f,
                             AttributeModifier.Operation.ADD_MULTIPLIED_TOTAL));
 
+    // 混乱
     public static final Holder<MobEffect> CONFUSION = MOB_EFFECTS.register("confusion",
             () -> new ConfusionEffect(MobEffectCategory.HARMFUL, 0xe55590));
 
+    // 不屈
     public static final Holder<MobEffect> UNYIELDING = MOB_EFFECTS.register("unyielding",
             () -> new NormalEffect(MobEffectCategory.BENEFICIAL, 0x828a9b));
 
