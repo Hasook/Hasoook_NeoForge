4d7580d86b338400844d30b52511178982d3d726
Hasok <2656053560@qq.com>
2025-01-02 23:57:26 +0800

    去城市化

---
+++ src/main/java/com/hasoook/hasoookmod/event/enchantment/DeUrbanization.java
@@ -0,0 +1,9 @@
+package com.hasoook.hasoookmod.event.enchantment;
+
+import com.hasoook.hasoookmod.HasoookMod;
+import net.neoforged.fml.common.EventBusSubscriber;
+
+@EventBusSubscriber(modid = HasoookMod.MOD_ID)
+public class DeUrbanization {
+
+}
