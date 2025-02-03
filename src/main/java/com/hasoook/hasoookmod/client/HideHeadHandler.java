abf1ca62a3439148df1c218445b1ce40c62e9462
Hasok <2656053560@qq.com>
2025-02-04 01:05:56 +0800

    会导致附魔台不能附魔出某魔咒，所以先删了

--- src/main/java/com/hasoook/hasoookmod/client/HideHeadHandler.java
+++ src/main/java/com/hasoook/hasoookmod/client/HideHeadHandler.java
@@ -13,14 +13,14 @@ import java.lang.reflect.Field;
 public class HideHeadHandler {
     @SubscribeEvent
     public static void onRenderLiving(RenderLivingEvent.Pre<?, ?> event) {
-        // 直接检查并隐藏通用模型的头部
+        /*// 直接检查并隐藏通用模型的头部
         if (event.getRenderer().getModel() instanceof HumanoidModel<?> humanoidModel) {
             humanoidModel.head.visible = false; // 隐藏头
             humanoidModel.hat.visible = false; // 隐藏帽子层
         }
 
         // 其他生物模型的适配（例如非人形生物）
-        tryHideHeadByReflection(event.getRenderer().getModel());
+        tryHideHeadByReflection(event.getRenderer().getModel());*/
     }
 
     // 通过反射处理非人形模型的头部
